5253ab5a26beef067615edbbf5db4f3376a543ee
Garethlomax <gcl15@ic.ac.uk>
2020-04-22 14:13:55 +0100

    last branch before switching to linnear implementation

--- tlstm.py
+++ tlstm.py
@@ -7,17 +7,58 @@ This is a temporary script
 """
 
 import torch
-import torch.nn as nn
+import torch.nn
 import torch.nn.functional as F
 
-class Tlstm_unit(nn.Module):
+class TLSTM(nn.Module):
+
+
+class TLSTM_UNIT(nn.Module):
     """Base unit for T_lstm
     """
     
-    def __init__(self, hid_mem, concat_input):
+    def __init__(self, input_size, hidden_size):
         
         # 
-        super(Tlstm_unit, self).__init__()
+        super(TLSTM_UNIT, self).__init__()
         
-    def forward(self,x, hidden)
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.fc_size = fc_size
+        
+        # initialise weights for NNs.
+        # TODO: why not use linnear layers? 
+        # TODO: simplify into fewer layers - can superimpose
+        
+        self.Wi = nn.Parameter(torch.Tensor(self.input_size, self.hidden_size))
+        self.Ui = nn.Parameter(torch.Tensor(self.hidden_size, self.hidden_size))
+        # TODO: add biases 
+        
+        self.Wf = nn.Parameter(torch.Tensor(self.input_size, self.hidden_size))
+        self.Uf = nn.Parameter(torch.Tensor(self.hidden_size, self.hidden_size))
+        # TODO: add biases 
+        
+        # output
+        self.Wog = nn.Parameter(torch.Tensor(self.input_size, self.hidden_size))
+        self.Uog = nn.Parameter(torch.Tensor(self.hidden_size, self.hidden_size))
+        # TODO: add biases 
+        
+        self.Wc = nn.Parameter(torch.Tensor(self.input_size, self.hidden_size))
+        self.Uc = nn.Parameter(torch.Tensor(self.hidden_size, self.hidden_size))
+        # TODO: add biases 
+        
+        # TODO: figure out what issue is with 
+        self.W_decomp = nn.Parameter(torch.Tensor(self.hidden_size, self.hidden_size))
+        # TODO: add biases
+        
+        self.WoFC = nn.Parameter(torch.Tensor(self.hidden_size, self.fc_size))
+        # TODO: add biases
+        
+        
+        
+        
+        
+        
+    def forward(self,x, hidden):
+        
 
