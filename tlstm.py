959e30d03f82e9f892d9aee7e86d72520a232b7c
Garethlomax <gcl15@ic.ac.uk>
2020-04-22 15:42:13 +0100

    changed to linenar layer and added time delay

--- tlstm.py
+++ tlstm.py
@@ -17,7 +17,7 @@ class TLSTM_UNIT(nn.Module):
     """Base unit for T_lstm
     """
     
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, input_size, hidden_size, bias = True, time_delay_type = 'log'):
         
         # 
         super(TLSTM_UNIT, self).__init__()
@@ -25,40 +25,111 @@ class TLSTM_UNIT(nn.Module):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.fc_size = fc_size
-        
+        self.bias = bias
+        self.time_delay_type = time_delay_type
+        self.tanh = nn.tanh
         # initialise weights for NNs.
         # TODO: why not use linnear layers? 
         # TODO: simplify into fewer layers - can superimpose
+        # TODO: reframe to two linnear operations
         
-        self.Wi = nn.Parameter(torch.Tensor(self.input_size, self.hidden_size))
-        self.Ui = nn.Parameter(torch.Tensor(self.hidden_size, self.hidden_size))
-        # TODO: add biases 
         
-        self.Wf = nn.Parameter(torch.Tensor(self.input_size, self.hidden_size))
-        self.Uf = nn.Parameter(torch.Tensor(self.hidden_size, self.hidden_size))
-        # TODO: add biases 
+        # TODO: get rid of half of the biases. 
+        self.Wi = nn.linnear(self.input_size, self.hidden_size, bias = bias)
+        self.Ui = nn.linnear(self.hidden_size, self.hidden_size, bias = bias)
         
-        # output
-        self.Wog = nn.Parameter(torch.Tensor(self.input_size, self.hidden_size))
-        self.Uog = nn.Parameter(torch.Tensor(self.hidden_size, self.hidden_size))
-        # TODO: add biases 
+        self.Wf = nn.linnear(self.input_size, self.hidden_size, bias = bias)
+        self.Uf = nn.linnear(self.hidden_size, self.hidden_size, bias = bias)
+
+        #output
+        self.Wog = nn.linnear(self.input_size, self.hidden_size, bias = bias)
+        self.Uog = nn.linnear(self.hidden_size, self.hidden_size, bias = bias)
         
-        self.Wc = nn.Parameter(torch.Tensor(self.input_size, self.hidden_size))
-        self.Uc = nn.Parameter(torch.Tensor(self.hidden_size, self.hidden_size))
-        # TODO: add biases 
+        
+        self.Wc = nn.linnear(self.input_size, self.hidden_size, bias = bias)
+        self.Uc = nn.linnear(self.hidden_size, self.hidden_size, bias = bias)
         
         # TODO: figure out what issue is with 
-        self.W_decomp = nn.Parameter(torch.Tensor(self.hidden_size, self.hidden_size))
-        # TODO: add biases
+        self.W_decomp = nn.linnear(self.hidden_size, self.hidden_size, bias = bias)
         
-        self.WoFC = nn.Parameter(torch.Tensor(self.hidden_size, self.fc_size))
-        # TODO: add biases
+        self.WoFC = nn.linnear(self.hidden_size, self.fc_size, bias = bias)
         
-        
-        
-        
+        # TODO: figure out what softmax layer output is
+        # self.W_softmax = self.init_weights(fc_dim, output_dim, name='Output_Layer_weight',reg=None)
+                                               
         
         
-    def forward(self,x, hidden):
+        
+    def time_delay(self, t):
+        """ calculates time delay between input samples according to different
+        rules"""
+        
+        if time_delay_type == 'log': 
+            #TODO: redo this with pytorch constants ect to make static and faster
+            
+            T = 1 / np.log(np.e + t)
+            
+            T_vec = torch.full(self.hidden_size, fill_value = T)
+            # TODO: check if needs requires grad for differentiability.
+            
+            return T_vec
+        
+        elif time_delay_type == 'reciprocal':
+            
+            T_vec = torch.full(self.hidden_size, fill_value = 1/ t)
+            
+            return T_vec
+        
+    
+        
+        
+        
+        
+    def forward(self,x, t, hidden, cell):
+        """hidden and cell are previous hidden memory and cell state"""
+        
+        T = self.time_delay(t)
+        
+        # if there is a time delay decompose by time delay
+        C_ST = self.tanh(self.W_decomp(cell))
+        # discount short term cell memory
+        C_ST_dis = T * C_ST
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
 
